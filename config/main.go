88fbd7388cb9bd566bcea687ec9fc4d61625c773
Hero <2861061959@qq.com>
2025-06-26 22:53:27 +0800

    feat: add config and load InitConfig

---
+++ config/main.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Server   ServerConfig
+	Database DatabaseConfig
+}
+
+type ServerConfig struct {
+	Port int
+}
+
+type DatabaseConfig struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+	Name     string
+}
+
+var Cfg Config
+
+func InitConfig() {
+	viper.SetConfigName("config") // 不带扩展名
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".") // 当前目录
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Printf("读取配置失败: %v", err)
+	}
+
+	err = viper.Unmarshal(&Cfg)
+	if err != nil {
+		log.Printf("配置文件解析失败: %v", err)
+	}
+}
