07958ab51212693d24e7c6484058c522aca5755c
Hero <2861061959@qq.com>
2025-06-27 22:53:11 +0800

    feat: add swagger API

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,25 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// @title           blog API
+// @version         1.0
+// @description     This is a blog server celler server.
+
+// @contact.name   API Support
+// @contact.url    http://www.go-blog.io/support
+// @contact.email  support@blog.io
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:8080
+// @BasePath  /api/v1
+
+// @securityDefinitions.basic  BasicAuth
+
+// @externalDocs.description  OpenAPI
+// @externalDocs.url          https://blog.io/resources/open-api/
+
 func main() {
 	router := gin.Default()
 	router.Run(":8080")
